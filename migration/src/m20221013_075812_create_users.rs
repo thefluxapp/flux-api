4024f6dd7f556702cd3f735cd23700d0309e5b96
Andy Braga <bragovo@gmail.com>
2022-10-13 12:29:09 +0300

    Add user migration

---
+++ migration/src/m20221013_075812_create_users.rs
@@ -0,0 +1,40 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key()
+                            .extra("default uuid_generate_v4()".to_string())
+                            ,
+                    )
+                    .col(ColumnDef::new(User::Username).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Username,
+}
