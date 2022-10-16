7cfd06fa109aea05f1a02f5481aa2c7d42335fec
Andy Braga <bragovo@gmail.com>
2022-10-16 15:14:49 +0300

    Add messages migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20221013_075812_create_users;
+mod m20221016_114137_create_messages;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20221013_075812_create_users::Migration),
+            Box::new(m20221016_114137_create_messages::Migration)
         ]
     }
 }
