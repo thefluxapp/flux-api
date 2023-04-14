2b1fd8b398963e1d01fc8115a2c8756a208c5998
Andy Braga <bragovo@gmail.com>
2023-04-14 23:36:00 +0300

    Move tracing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use dotenv::dotenv;
-use tracing_subscriber::{self, EnvFilter};
 
 mod app;
+mod tracing;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .pretty()
-        .init();
+
+    tracing::run();
 
     app::run().await;
 }
---
+++ src/tracing/mod.rs
@@ -0,0 +1,21 @@
+use std::env;
+use tracing_subscriber::EnvFilter;
+
+pub fn run() {
+    let is_production =
+        env::var("APP_ENV").unwrap_or(String::from("development")) == String::from("production");
+
+    if is_production {
+        tracing_subscriber::fmt()
+            .with_thread_names(true)
+            .with_env_filter(EnvFilter::from_default_env())
+            .json()
+            .init();
+    } else {
+        tracing_subscriber::fmt()
+            .with_thread_names(true)
+            .with_env_filter(EnvFilter::from_default_env())
+            .pretty()
+            .init();
+    };
+}
