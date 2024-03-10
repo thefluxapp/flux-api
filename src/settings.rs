14c5eca5b14afb7c6f0561fc82893b03c5a4b1d3
Andy Braga <bragovo@gmail.com>
2024-03-10 19:43:10 +0300

    Set app directory for config

--- src/settings.rs
+++ src/settings.rs
@@ -20,10 +20,11 @@ pub struct Settings {
 
 pub fn new() -> Result<Settings, ConfigError> {
     let app_env = env::var("APP_ENV").unwrap_or_else(|_| "development".into());
+    let app_dir = env::var("APP_DIR").unwrap_or_else(|_| ".".into());
 
     let s = Config::builder()
-        .add_source(File::with_name("./config/default"))
-        .add_source(File::with_name(&format!("./config/{}", app_env)).required(false))
+        .add_source(File::with_name(&format!("{}/config/default", app_dir)))
+        .add_source(File::with_name(&format!("{}/config/{}", app_dir, app_env)).required(false))
         .add_source(Environment::with_prefix("app"))
         .set_override("env", app_env)?
         .build()?;
