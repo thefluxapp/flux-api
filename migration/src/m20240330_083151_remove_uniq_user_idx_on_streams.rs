e6e0c078d9c8ea295823642cbea3adee31e5f53d
Andy Braga <bragovo@gmail.com>
2024-03-30 11:35:23 +0300

    Remove user uniq idx

---
+++ migration/src/m20240330_083151_remove_uniq_user_idx_on_streams.rs
@@ -0,0 +1,13 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_index(Index::drop().name("idx_streams_user_id").to_owned())
+            .await
+    }
+}
